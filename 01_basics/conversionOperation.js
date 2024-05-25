7784c8efe59bc8c92ba997911edb29bac8fc216e
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-05-25 06:25:48 +0000

    conversion of datatpes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NAN
+//true=>1;false=>0
+
+let isLogin=1
+let bolleanIsLogin=Boolean(isLogin)
+console.log(bolleanIsLogin);
+
+//1=>true 0=>false
+//""=>false
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=14435
+let accountEmail='poornima@gmail.com'
+var accountPassword='1234'
+accountCity='pune'
+let accountState;
+//accountId=2
+accountEmail='dhdhebd'
+accountPassword='123332w2'
+accountCity='mumbai'
+console.log(accountId)
+
+/*
+Prefer bot use var
+because of issue in block scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
