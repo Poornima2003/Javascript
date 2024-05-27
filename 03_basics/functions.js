7a5ade4f75988c3310806a7b200c123f3a497940
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-05-27 12:48:22 +0000

    FUNCTIONS

---
+++ 03_basics/functions.js
@@ -0,0 +1,32 @@
+function sayMyName(){
+    console.log('p');
+    console.log('o');
+    console.log('o');
+    console.log('r');
+    console.log('n');
+    console.log('i');
+    console.log('m');
+    console.log('a')
+    
+}
+// sayMyName()
+
+function addTwoNumber(n01,n02){
+   console.log(n01+n02) 
+
+}
+addTwoNumber(3,4)
+
+function add(no1,no2){
+    // let result=no1+no2
+    // return result
+    return no1+no2
+}
+const result=add(3,2)
+console.log('result:',result)
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("poornima"))
+
