4a8cecff8b6ab59077999299d3b86749b97c91a1
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-05-29 08:34:15 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,26 @@
+//if
+const isUserLoggedIn=true
+if(2===2){
+console.log("execute")
+}
+
+const score=200
+if(score>100){
+    const power='fly'
+    console.log(`user power:${power}`
+    );
+
+}
+
+const balance=1000
+if(balance<500) {console.log("test");}
+else if(balance<750){
+    console.log("less than 750")
+}
+else if(balance<900){
+    console.log("less than 900")
+}
+else{
+    console.log("execute")
+}
+
