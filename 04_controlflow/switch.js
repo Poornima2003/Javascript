1f908f345736920461162ed0a11b5c114dc21f33
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-05-30 09:21:17 +0000

    control

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,25 @@
+const month=2
+
+switch (month) {
+    case 1:
+        console.log("janauary"
+        );
+        break;
+        case 2:
+        console.log("feburary"
+        );
+        break;
+        case 3:
+        console.log("march"
+        );
+        break;
+        case 4:
+        console.log("april"
+        );
+        break;
+        
+
+    default:
+        console.log("not in series")
+        break;
+}
